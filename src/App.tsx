906f1fd88f2203d4dcac217351c5b07f888c3bda
rene_mbp <harryloveslearning@googlemail.com>
2018-01-28 14:12:55 +0100

    all piano-sounds & play 4-octava

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,17 @@ import './App.css';
 
 import Keyboard from './Keyboard';
 
+import SoundPlayer from './utility/sound-player';
+
 class App extends React.Component {
+  soundPlayer: SoundPlayer;
+
+  constructor(props: React.Props<string>) {
+    super(props);
+
+    this.soundPlayer = new SoundPlayer();
+  }
+
   render() {
     return (
       <div className="App">
@@ -15,6 +25,38 @@ class App extends React.Component {
         </div>
       </div>
     );
+  }
+
+  componentWillMount() {
+    document.addEventListener('keydown', this.keyDown.bind(this), false);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.keyDown.bind(this), false);
+  }
+
+  keyDown(event: KeyboardEvent) {
+
+    const mapKeyToNote: object = {
+      'a': 'C4',
+      'w': 'C#4',
+      's': 'D4',
+      'e': 'D#4',
+      'd': 'E4',
+      'f': 'F4',
+      't': 'F#4',
+      'g': 'G4',
+      'z': 'G#4',
+      'h': 'A4',
+      'u': 'A#4',
+      'j': 'B4',
+      'k': 'C5'
+    };
+
+    const note = mapKeyToNote[event.key.toLowerCase()];
+    if (note) {
+      this.soundPlayer.play(note);
+    }
   }
 }
 
---
+++ src/utility/sound-player.ts
@@ -0,0 +1,51 @@
+import { Howl } from 'howler';
+
+class SoundPlayer {
+  sounds: {} = {};
+  notes: string[] = [];
+
+  constructor() {
+    this.setNotes();
+  }
+
+  play(note: string) {
+    const sound = this.getSound(note);
+    sound.play();
+  }
+
+  getSound(note: string): Howl {
+    note = note.toUpperCase();
+    let sound: Howl = this.sounds[note];
+    if (sound) {
+      return sound;
+    }
+
+    const noteFolder = './notes/';
+    const fileName = `${noteFolder}${this.notes.indexOf(note) + 1}-${note.replace('#', '-')}.mp3`;
+    sound = new Howl({
+      src: [fileName.toLowerCase()]
+    });
+    this.sounds[note] = sound;
+    return sound;
+  }
+
+  setNotes() {
+    const names = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
+
+    for (let octave = 0; octave < 8; octave++) {
+      let startIdx = 0;
+      if (octave === 0) {
+        startIdx = names.indexOf('A');
+      }
+      for (let i = startIdx; i < names.length; i++) {
+        this.notes.push(`${names[i]}${octave}`);
+        if (octave === 8 && i === 0) {
+          // finish on C8
+          return;
+        }
+      }
+    }
+  }
+}
+
+export default SoundPlayer;
