4d68e2aa438cc62bdcdb69ba80f16c529567f87a
rene_mbp <harryloveslearning@googlemail.com>
2018-03-03 15:48:43 +0100

    add RealBook.pdf

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -13,11 +13,14 @@ class App extends React.Component {
       <Provider store={store}>
         <div className="App">
           <header className="App-header">
-            <h1 className="App-title">riffer</h1>
+            <h1 className="App-title">
+            <a href="./REALBK1.PDF">Real Book</a>
+            </h1>
           </header>
           <KeypressHandler />
           <div className="keyboard">
-            <Keyboard showNotes="C3 E4"/>
+            <p>use keys a,s,d,f,g,h,j,k</p>
+            <Keyboard showNotes="C3 E4" />
           </div>
         </div>
       </Provider>
